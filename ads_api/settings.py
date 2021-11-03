6188728635ff0e8b56160b04c8eca50c5a8ed600
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-03 21:19:29 +0700

    add: secrets setting

--- ads_api/settings.py
+++ ads_api/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7!^s)r55d9&#a88_@l6&35v#*n0a9y4101^9*456=@(o%m-w5j'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
