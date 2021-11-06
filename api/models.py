630102fe67b2df2a4759b2c1a177da24d5616cff
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-06 21:27:43 +0700

    feat: tambah file admin dan model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    password = models.CharField(max_length=16)
+    username = models.CharField(max_length=256)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __repr__(self):
+        return str(self.user_id)
+
+class Station(models.Model):
+    station_id = models.BigAutoField(primary_key=True)
+    station_name = models.CharField(max_length=256)
+
+    def __repr__(self):
+        return str(self.station_id)
+
+class Cycle(models.Model):
+    cycle_id = models.BigAutoField(primary_key=True)
+    borrowed_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    station_id = models.ForeignKey(Station, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return str(self.cycle_id)
