8c0710c2fa51c8055b4a6c4dc912532e4fce89ef
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-08 20:10:41 +0700

    feat: tambah file untuk deployment

--- ads_api/settings.py
+++ ads_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 from dotenv import load_dotenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,3 +140,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
