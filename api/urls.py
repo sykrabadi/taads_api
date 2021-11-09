dab73da43705de3ec42dd5ec18f6d6432a98af27
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-09 21:20:45 +0700

    add: konfigurasi url

--- api/urls.py
+++ api/urls.py
@@ -2,18 +2,18 @@ from django.urls import path, register_converter
 from . import views
 
 
-# urlpatterns = [
-#     path("users/", views.Users.as_view()),
-#     path("user/<int:user_id>", views.UserDetail.as_view()),
-#     path("station/", views.get_station),
-#     path("cycle/", views.get_cycle)
-# ]
-
 urlpatterns = [
-    path("users/", views.get_user),
-    path("users/<int:pk>", views.get_user_by_id),
-    path("users/register", views.register),
-    path("users/delete/<int:pk>", views.delete_user),
+    path("users/", views.Users.as_view()),
+    path("user/<int:user_id>", views.UserDetail.as_view()),
     path("station/", views.get_station),
     path("cycle/", views.get_cycle)
 ]
+
+# urlpatterns = [
+#     path("users/", views.get_user),
+#     path("users/<int:pk>", views.get_user_by_id),
+#     path("users/register", views.register),
+#     path("users/delete/<int:pk>", views.delete_user),
+#     path("station/", views.get_station),
+#     path("cycle/", views.get_cycle)
+# ]
